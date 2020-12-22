1b20e02ede6d970d2da00c25ad8cce4dfd82229c
PM đình khang <khangdinhle123321@gmail.com>
2020-12-22 11:04:24 +0700

    xu li gio hang, homeview, tat ca danh muc

---
+++ WebBanDienThoaiCuoiKi/src/Servlets/PhuKienListServlet.java
@@ -0,0 +1,66 @@
+package servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.PC;
+import beans.PhuKien;
+import utils.DBUtils;
+import utils.MyUtils;
+
+/**
+ * Servlet implementation class PhuKienListServlet
+ */
+@WebServlet(name ="PKList", urlPatterns = {"/PKList"})
+public class PhuKienListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PhuKienListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Connection conn = MyUtils.getStoredConnection(request);
+		String tenkhachhang = request.getParameter("tenkhachhang");
+		String tenTK = request.getParameter("tenTK");
+		String errorString =null;
+		List<PhuKien> listPK = null;
+		try {
+			listPK=DBUtils.listPhuKien(conn);
+		}catch(SQLException e) {
+			e.printStackTrace();
+			errorString = e.getMessage();
+		}
+		request.setAttribute("PKList", listPK);
+		request.setAttribute("tenkhachhang", tenkhachhang);
+		request.setAttribute("tenTK", tenTK);
+		RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher("/WEB-INF/views/PhuKienListView.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
